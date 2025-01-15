ff6acfce8782bbd8238a1af046d2aaab37af2b6d
wenxiang.qian <wenxiang.qian@amlogic.com>
2025-01-15 19:16:32 +0800

    uploade first version

---
+++ common/build-info.cpp
@@ -0,0 +1,4 @@
+int LLAMA_BUILD_NUMBER = 4385;
+char const *LLAMA_COMMIT = "32d6ee63";
+char const *LLAMA_COMPILER = "cc (Ubuntu 13.3.0-6ubuntu2~24.04) 13.3.0";
+char const *LLAMA_BUILD_TARGET = "x86_64-linux-gnu";
---
+++ examples/gen-docs/CMakeFiles/llama-gen-docs.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/llama-gen-docs.dir/gen-docs.o"
+  "CMakeFiles/llama-gen-docs.dir/gen-docs.o.d"
+  "llama-gen-docs"
+  "llama-gen-docs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/llama-gen-docs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
