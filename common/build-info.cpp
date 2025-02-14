d422be15e68c6a6d4e18a6dcb0110b4649767ea5
wenxiang.qian <wenxiang.qian@amlogic.com>
2025-02-14 17:36:38 +0800

    fix log filter display

--- common/build-info.cpp
+++ common/build-info.cpp
@@ -1,4 +1,4 @@
-int LLAMA_BUILD_NUMBER = 13;
-char const *LLAMA_COMMIT = "120f630";
+int LLAMA_BUILD_NUMBER = 14;
+char const *LLAMA_COMMIT = "bab0408";
 char const *LLAMA_COMPILER = "cc (Ubuntu 13.3.0-6ubuntu2~24.04) 13.3.0";
 char const *LLAMA_BUILD_TARGET = "x86_64-linux-gnu";
