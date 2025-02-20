a1c136412243fffb50f2c54f4bc6327a8be86ed2
wenxiang.qian <wenxiang.qian@amlogic.com>
2025-02-20 19:52:40 +0800

    delete some file, example prompts ...

--- common/build-info.cpp
+++ common/build-info.cpp
@@ -1,4 +1,4 @@
-int LLAMA_BUILD_NUMBER = 14;
-char const *LLAMA_COMMIT = "bab0408";
+int LLAMA_BUILD_NUMBER = 15;
+char const *LLAMA_COMMIT = "d422be1";
 char const *LLAMA_COMPILER = "cc (Ubuntu 13.3.0-6ubuntu2~24.04) 13.3.0";
 char const *LLAMA_BUILD_TARGET = "x86_64-linux-gnu";
